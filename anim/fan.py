b96dbfd54f2ea5d0b645c3ec1c52b2477f8274dc
c1919 <superneonbros14@gmail.com>
2024-09-03 20:12:02 +0700

    added main folder+fan.py

---
+++ anim/fan.py
@@ -0,0 +1,36 @@
+#file: .../anim/fan.py
+#author: _cWorksLLC
+#desc: Propeller/Fan Animation.
+
+import time
+
+while 1==1:
+    f1l1 = "\        /"
+    f1l2 = "  \    /  "
+    f1l3 = "    O     "
+    f1l4 = "  /    \  "
+    f1l5 = "/       \  "
+    
+    print(f1l1)
+    print(f1l2)
+    print(f1l3)
+    print(f1l4)
+    print(f1l5)
+    
+    time.sleep(0.1)
+    
+    f2l1 = "     |    "
+    f2l2 = "     |    "
+    f2l3 = " ----O----"
+    f2l4 = "     |    "
+    f2l5 = "     |    "
+    
+    print(f2l1)
+    print(f2l2)
+    print(f2l3)
+    print(f2l4)
+    print(f2l5)
+    
+    time.sleep(0.1)
+    
+    #end
