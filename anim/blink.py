0a1059b2eb2f00a58a194cefb5c73547d9cc17c5
c1919 <superneonbros14@gmail.com>
2024-09-04 16:18:34 +0700

    added new animation

---
+++ anim/blink.py
@@ -0,0 +1,35 @@
+#file: .../anim/blink.py
+#author: _cWorksLLC
+#desc: Blinking Animation.
+
+import time
+
+while 1==1:
+    f1l1 = "/---\ "
+    f1l2 = "IO OI"
+    f1l3 = "I ~ I"
+    f1l4 = "I\_/I"
+    f1l5 = "\___/"
+    
+    print(f1l1)
+    print(f1l2)
+    print(f1l3)
+    print(f1l4)
+    print(f1l5)
+    
+    time.sleep(1)
+    
+    f2l1 = "/---\ "
+    f2l2 = "I- -I"
+    f2l3 = "I ~ I"
+    f2l4 = "I\_/I"
+    f2l5 = "\___/"
+    
+    print(f2l1)
+    print(f2l2)
+    print(f2l3)
+    print(f2l4)
+    print(f2l5)
+    
+    time.sleep(0.25)
+    
